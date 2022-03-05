121512c9d65e823cca62bf9613afcdbce62b4c84
Paulo Uilian <uilianlbarros@gmail.com>
2022-03-05 15:34:02 -0300

    implementará todas as funcionalidadas dos eventos de mouse

--- script/main.js
+++ script/main.js
@@ -1,32 +1,43 @@
-
+let currentEvent;
 
 const comands = {
     mousedown: {
         img: './img/img_code/click.png',
-        conteiner_div: `<button>Clique aqui</button>`,
-        section: ''
+        conteiner_div: `<button>Clique</button>`,
+        section: '',
+        msg: 'Algum botão do mouse foi clicado'
     },
     click: {
         img: './img/img_code/click.png',
-        conteiner_div: `<button>Clique aqui</button>`,
-        section: ''
+        conteiner_div: `<button>Clique com o botão esquerdo</button>`,
+        section: '',
+        msg: 'O botão esquerdo do mouse foi clicado'
     },
     dblclick: {
         img: './img/img_code/click.png',
-        conteiner_div: `<button>Clique aqui</button>`,
-        section: ''
+        conteiner_div: `<button>Clique duas vezes</button>`,
+        section: '',
+        msg: 'Foi clicado duas vezes seguidas o botão esquerdo do mouse'
+    },
+    contextmenu: {
+        img: './img/img_code/click.png',
+        conteiner_div: `<button>Clique com o botão direito</button>`,
+        section: '',
+        msg: 'Foi clicado o botão direito do mouse'
     }
 }
 
 const listOfOptions = {
     mouse: {
-        mousedown: {img: 'mouse'},
-        click: {img: 'leftclickmouse'},
-        dblclick: {img: 'leftclickmouse'},
-        contextmenu: {img: 'rightclickmouse'}
+        mousedown: {icon: 'mouse'},
+        click: {icon: 'leftclickmouse'},
+        dblclick: {icon: 'leftclickmouse'},
+        contextmenu: {icon: 'rightclickmouse'}
     }
 }
 
+const buttons = [...document.querySelectorAll('header nav:first-child button')];
+addEvents(buttons);
 
 /* Main logic */
 
@@ -38,7 +49,7 @@ function assembleSubButtons(mainButton){
     for(const option in listOfOptionsBtnMain){
         secondNavArea.innerHTML += `
             <button id="${option}">
-                <img src="./img/${listOfOptionsBtnMain[option].img}.png" alt="botão"></img>
+                <img src="./img/${listOfOptionsBtnMain[option].icon}.png" alt="botão"></img>
                 ${option}
             </button>`;
     }
@@ -48,8 +59,8 @@ function assembleSubButtons(mainButton){
 }
 
 function subButtons(btnClickedId){
+    currentEvent = btnClickedId;
     for (const id in comands) {
-        console.log(id);
         if(id === btnClickedId){
             makeScreen(comands[id]);
         }
@@ -62,14 +73,18 @@ function makeScreen(obj){
 
     codeSide.innerHTML = `<img src="${obj.img}"></img>`;
     eventSide.innerHTML = `<div>${obj.conteiner_div}</div>`;
-    eventSide.innerHTML += `<section>${obj.section}</section>`;
+    eventSide.innerHTML += `<section><p><span>~ console:</span>${obj.section}</p></section>`;
+
+    upDateElements();
 }
 
 
 /* Eventos */
 
-const buttons = [...document.querySelectorAll('header nav:first-child button')];
-addEvents(buttons);
+function upDateElements(){
+    const button = document.querySelector('.dir .conteiner button');
+    button.addEventListener(currentEvent, simulateEvent, false);
+}
 
 function addEvents(elements){
     for (const index in elements) {
@@ -100,4 +115,17 @@ function eventsListener(event){
 
         default: subButtons(id);
     }
+}
+
+function simulateEvent(event){
+    event.stopPropagation();
+    const console = document.querySelector('.dir section p');
+    console.innerHTML += `<span>${comands[currentEvent].msg}</span>`;
+
+    setTimeout(() => {
+        console.innerHTML += '<span>limpando...</span>';
+        setTimeout(() => {          
+            console.innerHTML = '<span>~ console:</span>';
+        }, 1200);
+    }, 3000);
 }
